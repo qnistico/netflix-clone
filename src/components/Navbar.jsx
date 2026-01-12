35a3c89c697e835f22066422fa01437c8a7d31d0
qnistico <quintonnistico@gmail.com>
2026-01-12 15:31:09 -0500

    bugfix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,7 +101,7 @@ function Navbar() {
                   }}
                   className="ml-2 hover:text-gray-300 transition-all duration-200 hover:scale-110 active:scale-95"
                 >
-                  <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <svg className="cursor-pointer w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                   </svg>
                 </button>
